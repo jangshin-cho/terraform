cf455e4f1eccb45bcac9694f4929166280025e14
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-04-29 10:45:18 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,11 +8,16 @@ terraform {
 }
 
 # Configure the Azure provider
-provider "azurerm" {
+#provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
   #version = "~>3.0"
-  features {}
+#  features {}
+#}
+
+provider "azuread" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider to be used
+  version = "=1.1.0"
 }
 
 resource "azurerm_resource_group" "terraform-demo" {
