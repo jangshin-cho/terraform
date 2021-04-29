a341781d08bb30843649f06145cd9dd99f0701d6
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-04-29 16:20:02 +0900

    Update vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -3,7 +3,7 @@ resource "azurerm_virtual_network" "terraformnetwork" {
     name = "myVnet"
     address_space = ["10.0.0.0/16"]
     location = "koreacentral"
-    resource_group_name = "axin00terraform"
+    resource_group_name = "terraform-demo"
 
     tags = {
       environment = "Terraform Test"
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "terraformnetwork" {
 # Create subnet
 resource "azurerm_subnet" "terraformsubnet" {
     name                 = "choSubnet"
-    resource_group_name  = "axin00terraform"
+    resource_group_name  = "terraform-demo"
     virtual_network_name = "azureterraformvnet"
     address_prefixes     = ["10.0.1.0/24"]
 }
