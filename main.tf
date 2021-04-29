0db3cbf1aa6d7865098aa57aa498a8a951736ab0
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-04-29 10:54:00 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,12 +18,12 @@ provider "azuread" {
 
 
 # Configure the Azure provider
-provider "azurerm" {
+#provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
   #version = "~>3.0"
-  features {}
-}
+#  features {}
+#}
 
 
 
