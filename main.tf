e61fd185a7d45fc898c8816e8ff10d52d9bc306f
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-05-03 16:31:28 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
   #version = "~>3.0"
-  #client_id     = ${client_id}
-  #client_secret = ${client_secret}
-  #tenant_id     = ${tenant_id}
-  #subscription_id = ${subscription_id}
+  client_id     = "0a9b6acf-87fc-44e5-95d6-1170f2dcce27"
+  client_secret = "Lz.2UycBgLktW1fGoc65P5HMM4FUNC9OTQ"
+  tenant_id     = "cdbb77be-38b6-40d1-b078-b53f5d96ca61"
+  subscription_id = "52487da4-ef68-4ad9-a9d9-05105aa35625"
   features {}
 }
 
