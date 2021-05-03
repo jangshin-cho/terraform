289fca186236e6ec1bc81c2529a55d6a38cb4f85
jangshin-cho <83290851+jangshin-cho@users.noreply.github.com>
2021-05-03 16:40:35 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+variable "cloud_id" {}
+variable "cloud_secret" {}
+variable "cloud_tid" {}
+variable "cloud_sid" {}
+
 terraform {
   backend "azurerm" {
     resource_group_name   = "terraform01"
@@ -23,10 +28,10 @@ provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you are using version 1.x, the "features" block is not allowed.
   #version = "~>3.0"
-  client_id     = $cloud_id
-  client_secret = $cloud_secret
-  tenant_id     = $cloud_tid
-  subscription_id = $cloud_sid
+  client_id     = "${var.cloud_id}"
+  client_secret = "${var.cloud_secret}"
+  tenant_id     = "${var.cloud_tid}"
+  subscription_id = "${var.cloud_sid}"
   features {}
 }
 
